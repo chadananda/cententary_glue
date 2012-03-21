310e611c8e494a6ffc4f4a6669036f0009e768fb
David Hunt <git@dnotes.net>
2012-03-21 19:05:53 -0400

    changes to image spacing on daily view

--- dailyview.js
+++ dailyview.js
@@ -3,11 +3,11 @@ Drupal.behaviors.dailyview = function(context) {
   var sideht = $(".view-display-id-attachment_2").height();
   if (mainht > sideht) {
     htdiff = (mainht - sideht) % 200;
-    if (mainht - sideht < 19) {
-      $(".view-display-id-attachment_4").css('margin-bottom', htdiff + 'px');
+    if (htdiff < 182) {
+      $(".view-display-id-attachment_2").css('margin-bottom', (htdiff + 1) + 'px');
     }
-    else if (htdiff < 145) {
-      $(".view-display-id-attachment_2").css('margin-bottom', htdiff + 'px');
+    else {
+      $(".view-display-id-attachment_4").css('margin-bottom', (19 - (200 - htdiff)) + 'px');
     }
   }
   else {
