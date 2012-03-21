7383f6cc6405337f7b293d954167f3983600e7c2
David Hunt <git@dnotes.net>
2012-03-21 17:38:31 -0400

    changed daily view image alignment

--- dailyview.js
+++ dailyview.js
@@ -3,17 +3,15 @@ Drupal.behaviors.dailyview = function(context) {
   var sideht = $(".view-display-id-attachment_2").height();
   if (mainht > sideht) {
     htdiff = (mainht - sideht) % 200;
-    if (htdiff < 145) {
+    if (mainht - sideht < 19) {
+      $(".view-display-id-attachment_4").css('margin-bottom', htdiff + 'px');
+    }
+    else if (htdiff < 145) {
       $(".view-display-id-attachment_2").css('margin-bottom', htdiff + 'px');
     }
   }
-  else if (sideht > mainht) {
+  else {
     htdiff = (sideht - mainht) % 200;
-    if (htdiff > 55) {
-      $(".view-display-id-attachment_2").css('margin-bottom', (200 - htdiff) + 'px');
-    }
-    else {
-      $(".view-display-id-attachment_4").css('margin-bottom', (htdiff + 19) + 'px');
-    }
+    $(".view-display-id-attachment_2").css('margin-bottom', (200 - htdiff) + 'px');
   }
 }
