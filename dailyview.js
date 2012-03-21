5faafa5a36b55e3781c39aa16c285d7fbd4e9840
David Hunt <git@dnotes.net>
2012-03-21 17:21:54 -0400

    fixed numbers for image alignment

--- dailyview.js
+++ dailyview.js
@@ -2,15 +2,15 @@ Drupal.behaviors.dailyview = function(context) {
   var mainht = $(".view-display-id-attachment_4").height();
   var sideht = $(".view-display-id-attachment_2").height();
   if (mainht > sideht) {
-    newht = (mainht - sideht) % 200;
-    if (newht < 130) {
+    htdiff = (mainht - sideht) % 200;
+    if (htdiff < 145) {
       $(".view-display-id-attachment_2").css('margin-bottom', newht + 'px');
     }
   }
   else if (sideht > mainht) {
-    newht = (sideht - mainht) % 200;
-    if (newht > 60) {
-      $(".view-display-id-attachment_2").css('margin-bottom', newht + 'px');
+    htdiff = (sideht - mainht) % 200;
+    if (htdiff > 70) {
+      $(".view-display-id-attachment_2").css('margin-bottom', (200 - newht) + 'px');
     }
   }
 }
